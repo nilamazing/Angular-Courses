96e6dcfedbd87a1c391bb48459ad402953613d39
Dutta <NDUTTA@na.ad.guycarp.com>
2020-03-06 15:29:01 +0000

    Deleted node_modules

--- CourseMicroService/index.js
+++ CourseMicroService/index.js
@@ -8,14 +8,20 @@ const PORT=8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-if(courseData && courseData.COURSES){
+app.get('/',(req,res)=>{
+  res.redirect('/courses');
+});
+app.get('/courses',(req,res)=>{
+  if(courseData && courseData.COURSES){
     let keys = Object.keys(courseData.COURSES);
     if(keys.length>0){
       console.log("Valid Courses Payload");
       coursePayload.coursePayload.populateData();
-      let jsonCoursePayloadData = JSON.stringify(coursePayload.coursePayload);
-      console.log(jsonCoursePayloadData);
+      //let jsonCoursePayloadData = JSON.stringify(coursePayload.coursePayload);
+      res.status(200).json(coursePayload.coursePayload);
+      // console.log(jsonCoursePayloadData);
     }
-}
+  }
+});
 
-//app.listen(PORT,()=>{console.log(`The Server is up and listening on ${PORT}`)});
+app.listen(PORT,()=>{console.log(`The Server is up and listening on ${PORT}`)});
